06d4c3f0daa5194646ffacc4bca3f8ea471de222
lucasvinicode <130604012+lucasvinicode@users.noreply.github.com>
2023-04-12 16:58:31 -0300

    Add files via upload

---
+++ proxy.bat
@@ -0,0 +1,2 @@
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d 127.0.0.1:1010 /f
